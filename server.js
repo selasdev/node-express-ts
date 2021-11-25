b68df7d6b5fcaae5ff22df753c91bf4fcf323bfb
Carlos Rosales <carlosros033@gmail.com>
2021-11-24 22:53:53 -0400

    feat: obteniendo información del body y de la query)

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("root");
 });
 
 app.get("/message", (req, res) => {
+    console.log(req.query);
   res.send("lista de mensajes");
 });
 
 app.post("/message", (req, res) => {
+  console.log(req.body);
   res.send("creado nuevo mensaje");
 });
 
