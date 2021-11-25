fe95973ac7f4e24b35e37ebbef76282a4f829315
Carlos Rosales <carlosros033@gmail.com>
2021-11-24 23:23:22 -0400

    feat: agregando tipado al archivo de server

--- server.ts
+++ server.ts
@@ -1,26 +1,31 @@
-const express = require("express");
+import express, { Application, Request, Response } from "express";
 
-const app = express();
+const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (_: any, res: any) => {
+app.get("/", (_: Request, res: Response) => {
   res.send("root");
 });
 
-app.get("/message", (req: any, res: any) => {
+app.get("/message", (req: Request, res: Response) => {
   console.log(req.query);
   console.log(req.headers);
   res.header({
-      "custom-header": "valor custom"
-  })
+    "custom-header": "valor custom",
+  });
   res.send("lista de mensajes!");
 });
 
-app.post("/message", (req:any, res: any) => {
+app.post("/message", (req: Request, res: Response) => {
   console.log(req.body);
   res.send("creado nuevo mensaje");
 });
 
-app.listen(3000);
-console.log("La aplicación está escuchando en http://localhost:3000");
+try {
+  app.listen(3000, () => {
+    console.log("La aplicación está escuchando en http://localhost:3000");
+  });
+} catch (error) {
+  console.error("Algo sucedió tratando de oír el puerto:" + error);
+}
