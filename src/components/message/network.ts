a4648eff6c4911b5aee8bb9662caed1660fe5866
Carlos Rosales <carlosros033@gmail.com>
2021-11-27 21:11:58 -0400

    feat(files): agregando middleware para nombre de archivos

--- src/components/message/network.ts
+++ src/components/message/network.ts
@@ -1,48 +1,72 @@
 import { Router, Request, Response } from "express";
 import { successResponse, errorResponse } from "../../network/response";
-import { addMessage, deleteMessage, getMessages, patchMessage } from "./controller";
+import {
+  addMessage,
+  deleteMessage,
+  getMessages,
+  patchMessage,
+} from "./controller";
 const multer = require("multer");
+const path = require("path");
 const router = Router();
 
+const storage = multer.diskStorage({
+  destination: function (_: any, __: any, cb: any) {
+    cb(null, "public/files/");
+  },
+  filename: function (_: any, file: any, cb: any) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
 const upload = multer({
-  dest: 'public/files/',
-})
+  dest: "public/files/",
+  storage,
+});
 
 router.get("/", async (req: Request, res: Response) => {
   try {
     const userFilter = req.query?.user;
     const chatFilter = req.query?.chat;
-    const messages = await getMessages(userFilter as string, chatFilter as string);
+    const messages = await getMessages(
+      userFilter as string,
+      chatFilter as string
+    );
     successResponse(req, res, 200, { messages });
-  }catch(error){
-    errorResponse(req, res, 500, error)
+  } catch (error) {
+    errorResponse(req, res, 500, error);
   }
 });
 
-router.post("/", upload.single('file'), async (req: Request, res: Response) => {
-    try {
-      console.log((req as any).file);
-      const responseMessage = await addMessage(req.body?.chat, req.body?.user, req.body?.message, (req as any).file);
-      successResponse(req, res, 201, responseMessage);
-    } catch (error) {
-      errorResponse(req, res, 500, error)
-    }
+router.post("/", upload.single("file"), async (req: Request, res: Response) => {
+  try {
+    console.log((req as any).file);
+    const responseMessage = await addMessage(
+      req.body?.chat,
+      req.body?.user,
+      req.body?.message,
+      (req as any).file
+    );
+    successResponse(req, res, 201, responseMessage);
+  } catch (error) {
+    errorResponse(req, res, 500, error);
+  }
 });
 
-router.patch("/:id", async(req: Request, res: Response) => {
+router.patch("/:id", async (req: Request, res: Response) => {
   try {
     const responseMessage = await patchMessage(req.params?.id, req.body?.text);
     successResponse(req, res, 200, responseMessage);
-  }catch(error){
+  } catch (error) {
     errorResponse(req, res, 500, error);
   }
-})
+});
 
 router.delete("/:id", async (req: Request, res: Response) => {
-  try{
-    const responseMessage = await deleteMessage(req.params?.id)
-    successResponse(req, res, 200, responseMessage)
-  }catch(error) {
+  try {
+    const responseMessage = await deleteMessage(req.params?.id);
+    successResponse(req, res, 200, responseMessage);
+  } catch (error) {
     errorResponse(req, res, 500, error);
   }
 });
